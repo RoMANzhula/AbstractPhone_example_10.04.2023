8545e8d398bf94f3f20879dc89c1310ea766c97f
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 18:04:43 +0200

    Update User.java
    In Ukrainian

--- src/User.java
+++ src/User.java
@@ -1,11 +1,13 @@
-public class User { //публичный класс Пользователь
-    private String name; //приватное поле - имя
+public class User { // публічний клас Користувач
+    private String name; // приватне поле - ім'я
 
-    public User(String name) { //конструктор класса
-        this.name = name; //инициализаци поля класса
+    public User(String name) { // конструктор класу
+        this.name = name; // ініціалізація поля класу
     }
 
-    public void callAnotherUser(int number, AbstractPhone abstractPhone) { //вот явный пример использования ПОЛИМОРФИЗМА - AbstractPhone abstractPhone
-        abstractPhone.callTo(number); //обьект и метод класса AbstractPhone - выполняем звонок с номера
+    public void callAnotherUser(int number, AbstractPhone abstractPhone) { 
+        // ось явний приклад використання ПОЛІМОРФІЗМУ - AbstractPhone abstractPhone
+        abstractPhone.callTo(number); 
+        // об'єкт і метод класу AbstractPhone - здійснюємо дзвінок за номером
     }
 }
