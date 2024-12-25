02bcb2629990a78c603279523fe7c783edf84151
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 18:01:59 +0200

    Update Smartphone.java
    In Ukrainian

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,13 +1,13 @@
-public class Smartphone extends CellPhone { //публичный класс смартфон
-    private String operationSystem; //добавляем приватное поле - операционнаяСистема
+public class Smartphone extends CellPhone { //публічний клас Смартфон
+    private String operationSystem; //додаємо приватне поле - операційна система
 
-    ////обязательная реализация конструктора от класса-родителя
+    ////обов'язкова реалізація конструктора від класу-батька
     public Smartphone(int yearOfIssue, int hoursOfBattery, String operationSystem) {
-        super(yearOfIssue, hoursOfBattery); //наследуемые поля от класса-родителя
-        this.operationSystem = operationSystem; //инициализация поля класса
+        super(yearOfIssue, hoursOfBattery); //наслідувані поля від класу-батька
+        this.operationSystem = operationSystem; //ініціалізація поля класу
     }
 
-    public void install(String program) { //создаем метод для добавления программного обеспечения, на вход метод получает строковое название программы
-        System.out.println("Installing " + program + " for " + operationSystem); //печатаем - Устанавливаю программу для операционной системы
+    public void install(String program) { //створюємо метод для встановлення програмного забезпечення, на вхід метод отримує рядкову назву програми
+        System.out.println("Встановлюю " + program + " для " + operationSystem); //виводимо - Встановлюю програму для операційної системи
     }
 }
