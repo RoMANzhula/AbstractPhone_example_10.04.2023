cd839e2db3c3664c0bb3d0b98ea4c21927d55709
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 18:02:54 +0200

    Update SomePhone.java
    In Ukrainian

--- src/SomePhone.java
+++ src/SomePhone.java
@@ -1,20 +1,20 @@
-public class SomePhone { //публичный класс Какой-тоТелефон
-    private int yearIssue; //приватное поле годВыпуска
-    private String company; //приватное поле компания
+public class SomePhone { // публічний клас ЯкийсьТелефон
+    private int yearIssue; // приватне поле рікВипуску
+    private String company; // приватне поле компанія
 
-    public SomePhone(int yearIssue, String company) { //конструктор класса
-        this.yearIssue = yearIssue; //инициализация полей
+    public SomePhone(int yearIssue, String company) { // конструктор класу
+        this.yearIssue = yearIssue; // ініціалізація полів
         this.company = company;
     }
 
-    private void openConnection(){} //метод (пустой) открытьСоединение
+    private void openConnection() {} // метод (порожній) відкритиЗ'єднання
 
-    public void callTo() { //публичный метод звонитьКому-то
-        openConnection(); //вызываем метод открытьСоединение
-        System.out.println("I am calling number..."); //печатаем - я звоню по номеру
+    public void callTo() { // публічний метод дзвонитиКому-небудь
+        openConnection(); // викликаємо метод відкритиЗ'єднання
+        System.out.println("Я дзвоню за номером..."); // друкуємо - я дзвоню за номером
     }
 
-    public void callingUs() { //публичный метод звонятНам()
-        System.out.println("Ding-ding..."); //печатаем - дзынь-дзынь
+    public void callingUs() { // публічний метод дзвонятьНам()
+        System.out.println("Дзинь-дзинь..."); // друкуємо - дзинь-дзинь
     }
 }
